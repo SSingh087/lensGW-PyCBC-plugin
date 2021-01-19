cad45532b3599122ca71f0899c951286b076a45c
Shashwat Singh <shashwat98singh@gmil.com>
2021-01-19 20:36:19 +0530

    lgw working

--- build/lib/lgw.py
+++ build/lib/lgw.py
@@ -1,58 +1,32 @@
-def lensed_gw_fd():
+def lensed_gw_fd(**kwds):
     hp_tilde_lensed, hc_tilde_lensed = lensed_gw_td(loc_lensed, diff=None, 
                                     scaled= False, scale_factor=None, cosmo=None)
     return hp_tilde_lensed.to_frequencyseries(), hc_tilde_lensed.to_frequencyseries()
 
-def lensed_gw_td():
+def lensed_gw_td(y0 = 0.1,y1 = 0.7937005,l0 = 0.5,l1 = 0,zS = 2.0,zL = 0.5,mL1  = 1e6,
+                mL2  = 1e6,lens_model_list = ['POINT_MASS', 'POINT_MASS'],
+                approx='IMRPhenomD',mass1=100,mass2=100,spin1x=0.0,spin1y=0.0,
+                spin1z=0.9,spin2x=0.0,spin2y=0.0,spin2z=0.4,inclination=1.23,
+                distance=500,coa_phase=2.45,delta_t=1.0/16384,delta_f=16384,f_lower=30,
+                polarization=2.34, eccentricity=0,end_time=1192529720,**kwds):
     
     from lensGW.waveform.waveform_utils import lens_waveform_model
     from lensGW.amplification_factor.amplification_factor import geometricalOpticsMagnification
     from lensGW.utils.utils import get_lensed_gws
     from pycbc.types.timeseries import TimeSeries
     from pycbc import waveform
-    
-    approximant=args['approximant']
-    mass1=args['mass1']
-    mass2=args['mass2']
-    spin1x=args['spin1x']
-    spin1y=args['spin1y']
-    spin1z=args['spin1z']
-    spin2x=args['spin2x']
-    spin2y=args['spin2y']
-    spin2z=args['spin2z']
-    inclination=args['inclination']
-    distance=args['distance']
-    coa_phase=args['coa_phase']
-    delta_t=args['delta_t']
-    delta_f=args['delta_f']
-    f_lower=args['f_lower']
-    det=args['det']
-    end_time=args['end_time']
-    ra=args['ra']
-    dec=args['dec']
-    polarization=args['polarization']
-    eccentricity=args['eccentricity']
-    
-    y0 = args['y0']
-    y1 = args['y1']
-    l0 = args['l0']
-    l1 = args['l1']
-    zS = args['zS']
-    zL = args['zL']
-    mL1  = args['mL1']
-    mL2  = args['mL2']
-    lens_model_list = args['lens_model_list']
 
     Img_ra, Img_dec, source_pos_x, source_pos_y,\
     zL, zS, lens_model_list, kwargs_lens_list, mtot = lens_waveform_model(None).eval_param(
                                                         y0,y1,l0,l1,zS,zL,mL1,mL2,lens_model_list
                                                         )
-
-    hp, hc = get_td_waveform(approximant= approximant, mass1= mass1, mass2= mass2, distance=distance,
+    if 'approximant' in kwds:
+        kwds.pop("approximant")
+    hp, hc = waveform.get_td_waveform(approximant= approx, mass1= mass1, mass2= mass2, distance=distance,
                                          spin1z= spin1z,spin1x=spin1x,spin1y=spin1y,
                                          spin2z= spin2z,spin2x=spin2x,spin2y=spin2y,
                                          inclination= inclination, coa_phase= coa_phase,
-                                         delta_f= delta_f, f_lower= f_lower,
+                                         delta_t= delta_t, f_lower= f_lower,
                                          eccentricity = eccentricity,
                                         )
     hp.start_time += end_time
@@ -60,17 +34,17 @@ def lensed_gw_td():
     
     freq = waveform.utils.frequency_from_polarizations(hp, hc)
     Fmag = geometricalOpticsMagnification(freq.data,
-                                       Img_ra,Img_dec,
-                                       source_pos_x,source_pos_y,
-                                       zL,zS,
-                                       lens_model_list,
-                                       kwargs_lens_list,
-                                       diff         = diff,
-                                       scaled       = scaled,
-                                       scale_factor = scale_factor,
-                                       cosmo        = cosmo)
+                                           Img_ra,Img_dec,
+                                           source_pos_x,source_pos_y,
+                                           zL,zS,
+                                           lens_model_list,
+                                           kwargs_lens_list,
+                                           diff         = None,
+                                           scaled       = False,
+                                           scale_factor = None,
+                                           cosmo        = None)
     #------------return numpy values---------------#
-    hp_tilde_lensed, hc_tilde_lensed, _ = get_lensed_gws(Fmag, hp.data, hc.data, strain.data)
+    hp_tilde_lensed, hc_tilde_lensed = get_lensed_gws(Fmag, hp.data, hc.data, None)
 
     #------------convert to pycbc.TimeSeries---------------#
     hp_tilde_lensed = TimeSeries(hp_tilde_lensed, delta_t=hp.delta_t)
--- lgw.py
+++ lgw.py
@@ -5,10 +5,10 @@ def lensed_gw_fd(**kwds):
 
 def lensed_gw_td(y0 = 0.1,y1 = 0.7937005,l0 = 0.5,l1 = 0,zS = 2.0,zL = 0.5,mL1  = 1e6,
                 mL2  = 1e6,lens_model_list = ['POINT_MASS', 'POINT_MASS'],
-                approximant='IMRPhenomD',mass1=100,mass2=100,spin1x=0.0,spin1y=0.0,
+                approx='IMRPhenomD',mass1=100,mass2=100,spin1x=0.0,spin1y=0.0,
                 spin1z=0.9,spin2x=0.0,spin2y=0.0,spin2z=0.4,inclination=1.23,
                 distance=500,coa_phase=2.45,delta_t=1.0/16384,delta_f=16384,f_lower=30,
-                polarization=2.34, eccentricity=0,end_time=1192529720):
+                polarization=2.34, eccentricity=0,end_time=1192529720,**kwds):
     
     from lensGW.waveform.waveform_utils import lens_waveform_model
     from lensGW.amplification_factor.amplification_factor import geometricalOpticsMagnification
@@ -20,8 +20,9 @@ def lensed_gw_td(y0 = 0.1,y1 = 0.7937005,l0 = 0.5,l1 = 0,zS = 2.0,zL = 0.5,mL1  
     zL, zS, lens_model_list, kwargs_lens_list, mtot = lens_waveform_model(None).eval_param(
                                                         y0,y1,l0,l1,zS,zL,mL1,mL2,lens_model_list
                                                         )
-
-    hp, hc = waveform.get_td_waveform(approximant= approximant, mass1= mass1, mass2= mass2, distance=distance,
+    if 'approximant' in kwds:
+        kwds.pop("approximant")
+    hp, hc = waveform.get_td_waveform(approximant= approx, mass1= mass1, mass2= mass2, distance=distance,
                                          spin1z= spin1z,spin1x=spin1x,spin1y=spin1y,
                                          spin2z= spin2z,spin2x=spin2x,spin2y=spin2y,
                                          inclination= inclination, coa_phase= coa_phase,
