fb02335d354f04402dd0186af7a0f1cb9242c809
SSingh087 <shashwat98singh@gmail.com>
2021-01-24 20:31:27 +0530

    tests

--- build/lib/lgw.py
+++ build/lib/lgw.py
@@ -1,14 +1,42 @@
-def lensed_gw_fd(**kwds):
-    hp_tilde_lensed, hc_tilde_lensed = lensed_gw_td(loc_lensed, diff=None, 
-                                    scaled= False, scale_factor=None, cosmo=None)
+# Copyright (C) 2021  Shashwat Singh
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+def lensed_gw_fd(y0 = 0,y1 = 0,l0 = 0,l1 = 0,zS = 0,zL = 0,mL1  = 100,
+                mL2  = 100,lens_model_list = [],
+                approx='IMRPhenomD',mass1=50,mass2=50,spin1x=0.0,spin1y=0.0,
+                spin1z=0.0,spin2x=0.0,spin2y=0.0,spin2z=0.0,inclination=0.0,
+                distance=500,coa_phase=0.0,delta_t=1.0/4096,delta_f=4096,f_lower=30,
+                eccentricity=0,**kwds):
+
+    hp_tilde_lensed, hc_tilde_lensed = lensed_gw_td(y0 = 0,y1 = 0,l0 = 0,l1 = 0,zS = 0,zL = 0,mL1  = 100,
+                                                    mL2  = 100,lens_model_list = [],approx='IMRPhenomD',
+                                                    mass1=50,mass2=50,spin1x=0.0,spin1y=0.0,spin1z=0.0,spin2x=0.0,
+                                                    spin2y=0.0,spin2z=0.0,inclination=0.0,distance=500,
+                                                    coa_phase=0.0,delta_t=1.0/4096,delta_f=4096,f_lower=30,
+                                                    polarization=1., eccentricity=0,end_time=1192529720)
+    
     return hp_tilde_lensed.to_frequencyseries(), hc_tilde_lensed.to_frequencyseries()
 
-def lensed_gw_td(y0 = 0.1,y1 = 0.7937005,l0 = 0.5,l1 = 0,zS = 2.0,zL = 0.5,mL1  = 1e6,
-                mL2  = 1e6,lens_model_list = ['POINT_MASS', 'POINT_MASS'],
-                approx='IMRPhenomD',mass1=100,mass2=100,spin1x=0.0,spin1y=0.0,
-                spin1z=0.9,spin2x=0.0,spin2y=0.0,spin2z=0.4,inclination=1.23,
-                distance=500,coa_phase=2.45,delta_t=1.0/16384,delta_f=16384,f_lower=30,
-                polarization=2.34, eccentricity=0,end_time=1192529720,**kwds):
+
+def lensed_gw_td(y0 = 0,y1 = 0,l0 = 0,l1 = 0,zS = 0,zL = 0,mL1  = 100,
+                mL2  = 100,lens_model_list = [],
+                approx='IMRPhenomD',mass1=50,mass2=50,spin1x=0.0,spin1y=0.0,
+                spin1z=0.0,spin2x=0.0,spin2y=0.0,spin2z=0.0,inclination=0.0,
+                distance=500,coa_phase=0.0,delta_t=1.0/4096,delta_f=4096,f_lower=30,
+                polarization=1., eccentricity=0,end_time=1192529720,**kwds):
     
     from lensGW.waveform.waveform_utils import lens_waveform_model
     from lensGW.amplification_factor.amplification_factor import geometricalOpticsMagnification
@@ -29,9 +57,7 @@ def lensed_gw_td(y0 = 0.1,y1 = 0.7937005,l0 = 0.5,l1 = 0,zS = 2.0,zL = 0.5,mL1  
                                          delta_t= delta_t, f_lower= f_lower,
                                          eccentricity = eccentricity,
                                         )
-    hp.start_time += end_time
-    # hc.start_time += end_time ----------- this not required 
-    
+
     freq = waveform.utils.frequency_from_polarizations(hp, hc)
     Fmag = geometricalOpticsMagnification(freq.data,
                                            Img_ra,Img_dec,
@@ -52,5 +78,4 @@ def lensed_gw_td(y0 = 0.1,y1 = 0.7937005,l0 = 0.5,l1 = 0,zS = 2.0,zL = 0.5,mL1  
     hc_tilde_lensed = TimeSeries(hc_tilde_lensed, delta_t=hc.delta_t)
     hc_tilde_lensed.start_time = hc.start_time
 
-    #waveform.add_custom_waveform('test',waveform_gen,'time', force=True)
     return hp_tilde_lensed, hc_tilde_lensed
