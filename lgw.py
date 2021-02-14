b18ef680f56d9a5a086f7d32110fa752a34fa000
SSingh087 <shashwat98singh@gmail.com>
2021-02-14 11:50:24 +0530

    modulate

--- lgw.py
+++ lgw.py
@@ -1,40 +1,8 @@
 def lensed_gw_fd(**kwargs):
-
-    y0 = kwargs['y0']
-    y1 = kwargs['y1']
-    l0 = kwargs['l0']
-    l1 = kwargs['l1']
-    zS = kwargs['zS']
-    zL = kwargs['zL']
-    mL = kwargs['mL']
-    lens_model_list = kwargs['lens_model_list']
-    approx = kwargs['approx']
-    mass1 = kwargs['mass1']
-    mass2 = kwargs['mass2']
-    delta_f = kwargs['delta_f']
-    f_lower = kwargs['f_lower']
-    distance = kwargs['distance']
-    inclination = kwargs['inclination']
-    coa_phase = kwargs['coa_phase']
-    eccentricity = kwargs['eccentricity']
-    spin1x = kwargs['spin1x']
-    spin1y = kwargs['spin1y']
-    spin1z = kwargs['spin1z']
-    spin2x = kwargs['spin2x']
-    spin2y = kwargs['spin2y']
-    spin2z = kwargs['spin2z']
-
-    hp_tilde_lensed, hc_tilde_lensed = lensed_gw_td(
-                                y0 = y0, y1 = y1, l0 = l0, l1 = l1, zS = zS, zL = zL,
-                                mL = mL, lens_model_list = lens_model_list,
-                                approx = approx, mass1 = mass1, mass2 = mass2,
-                                delta_t = 1./delta_f, f_lower = f_lower,
-                                spin1x = spin1x, spin1y = spin1y, spin1z = spin1z,
-                                spin2x = spin2x, spin2y = spin2y, spin2z = spin2z, 
-                                inclination = inclination, distance = distance,
-                                coa_phase = coa_phase, eccentricity = eccentricity, 
-                                        )
-    
+    delta_t=1./kwargs['delta_f']
+    del kwargs['delta_f']
+    kwargs['delta_t'] = delta_t
+    hp_tilde_lensed, hc_tilde_lensed = lensed_gw_td(**kwargs)    
     return hp_tilde_lensed.to_frequencyseries(), hc_tilde_lensed.to_frequencyseries()
 
 def lensed_gw_td(**kwargs):
@@ -54,33 +22,16 @@ def lensed_gw_td(**kwargs):
     zL = kwargs['zL']
     mL = kwargs['mL']
     lens_model_list = kwargs['lens_model_list']
-    approx = kwargs['approx']
-    mass1 = kwargs['mass1']
-    mass2 = kwargs['mass2']
-    delta_t = kwargs['delta_t']
-    f_lower = kwargs['f_lower']
-    distance = kwargs['distance']
-    inclination = kwargs['inclination']
-    coa_phase = kwargs['coa_phase']
-    eccentricity = kwargs['eccentricity']
-    spin1x = kwargs['spin1x']
-    spin1y = kwargs['spin1y']
-    spin1z = kwargs['spin1z']
-    spin2x = kwargs['spin2x']
-    spin2y = kwargs['spin2y']
-    spin2z = kwargs['spin2z']
-    
+
     mL = array(mL,dtype=float64)
     Img_ra, Img_dec, source_pos_x, source_pos_y,\
                 _, _, _, kwargs_lens_list = lens_waveform_model(None).eval_param(
                                                             y0,y1,l0,l1,zS,zL,mL,lens_model_list)
-                                                            
-    hp, hc = waveform.get_td_waveform(approximant=approx, mass1=mass1, mass2=mass2, distance=distance,
-                                         spin1z=spin1z, spin1x=spin1x, spin1y=spin1y,
-                                         spin2z=spin2z, spin2x=spin2x, spin2y=spin2y,
-                                         inclination=inclination, coa_phase=coa_phase,
-                                         delta_t=delta_t, f_lower=f_lower, eccentricity=eccentricity,
-                                        )
+    if "approximant" in kwargs:
+        kwargs.pop("approximant")
+
+    hp, hc = waveform.get_td_waveform(approximant='IMRPhenomD', **kwargs)
+
     freq = waveform.utils.frequency_from_polarizations(hp, hc)
     Fmag = geometricalOpticsMagnification(freq.data,
                                            Img_ra,Img_dec,
