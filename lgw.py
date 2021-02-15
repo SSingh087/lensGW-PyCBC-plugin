ae9a5b5bc2914a3053ea47a59b3e8ef9421d6fb7
SSingh087 <shashwat98singh@gmail.com>
2021-02-15 17:03:11 +0530

    modular

--- lgw.py
+++ lgw.py
@@ -1,18 +1,11 @@
-def lensed_gw_fd(**kwargs):
-    delta_t=1./kwargs['delta_f']
-    del kwargs['delta_f']
-    kwargs['delta_t'] = delta_t
-    hp_tilde_lensed, hc_tilde_lensed = lensed_gw_td(**kwargs)    
-    return hp_tilde_lensed.to_frequencyseries(), hc_tilde_lensed.to_frequencyseries()
+from lensGW.waveform.waveform_utils import lens_waveform_model
+from lensGW.amplification_factor.amplification_factor import geometricalOpticsMagnification
+from lensGW.utils.utils import get_lensed_gws
+from pycbc.types.timeseries import TimeSeries
+from pycbc import waveform
+from numpy import array, float64
 
-def lensed_gw_td(**kwargs):
-    
-    from lensGW.waveform.waveform_utils import lens_waveform_model
-    from lensGW.amplification_factor.amplification_factor import geometricalOpticsMagnification
-    from lensGW.utils.utils import get_lensed_gws
-    from pycbc.types.timeseries import TimeSeries
-    from pycbc import waveform
-    from numpy import array, float64
+def get_lens_param(is_td=True, **kwargs):
     
     y0 = kwargs['y0']
     y1 = kwargs['y1']
@@ -22,16 +15,19 @@ def lensed_gw_td(**kwargs):
     zL = kwargs['zL']
     mL = kwargs['mL']
     lens_model_list = kwargs['lens_model_list']
-
     mL = array(mL,dtype=float64)
     Img_ra, Img_dec, source_pos_x, source_pos_y,\
                 _, _, _, kwargs_lens_list = lens_waveform_model(None).eval_param(
                                                             y0,y1,l0,l1,zS,zL,mL,lens_model_list)
+                
     if "approximant" in kwargs:
         kwargs.pop("approximant")
-
-    hp, hc = waveform.get_td_waveform(approximant='IMRPhenomD', **kwargs)
-
+    
+    if is_td:
+        hp, hc = waveform.get_td_waveform(approximant='IMRPhenomD', **kwargs)
+    else:
+        hp_fd, hc_fd = waveform.get_fd_waveform(approximant='IMRPhenomD', **kwargs)
+        hp, hc = hp_fd.to_timeseries(), hc_fd.to_timeseries()
     freq = waveform.utils.frequency_from_polarizations(hp, hc)
     Fmag = geometricalOpticsMagnification(freq.data,
                                            Img_ra,Img_dec,
@@ -47,4 +43,11 @@ def lensed_gw_td(**kwargs):
     hp_tilde_lensed.start_time += hp.start_time
     hc_tilde_lensed = TimeSeries(hc_tilde_lensed, delta_t=hc.delta_t)
     hc_tilde_lensed.start_time += hc.start_time
-    return hp_tilde_lensed, hc_tilde_lensed
+    return hp_tilde_lensed, hc_tilde_lensed  
+
+def lensed_gw_fd(**kwargs):
+    hp_tilde_lensed, hc_tilde_lensed = get_lens_param(is_td=False,**kwargs)
+    return hp_tilde_lensed.to_frequencyseries(), hc_tilde_lensed.to_frequencyseries()
+
+def lensed_gw_td(**kwargs):
+    return get_lens_param(is_td=True,**kwargs)
