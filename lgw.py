2ccd16bc2f39f063f7eee15006d7738817e47851
SSingh087 <shashwat98singh@gmail.com>
2021-02-19 18:01:55 +0530

    delta_f precision issue

--- lgw.py
+++ lgw.py
@@ -1,7 +1,7 @@
 from lensGW.waveform.waveform_utils import lens_waveform_model
 from lensGW.amplification_factor.amplification_factor import geometricalOpticsMagnification
 from lensGW.utils.utils import get_lensed_gws
-from pycbc.types.timeseries import TimeSeries
+from pycbc.types.timeseries import TimeSeries, FrequencySeries
 from pycbc import waveform
 from numpy import array, float64
 
@@ -25,9 +25,10 @@ def get_lens_param(is_td=True, **kwargs):
     
     if is_td:
         hp, hc = waveform.get_td_waveform(approximant='IMRPhenomD', **kwargs)
+        print(hp.delta_f)
     else:
         hp_fd, hc_fd = waveform.get_fd_waveform(approximant='IMRPhenomD', **kwargs)
-        hp, hc = hp_fd.to_timeseries(), hc_fd.to_timeseries()
+        hp, hc = hp_fd.to_timeseries(hp_fd.delta_t), hc_fd.to_timeseries(delta_t=hc_fd.delta_t)
     freq = waveform.utils.frequency_from_polarizations(hp, hc)
     Fmag = geometricalOpticsMagnification(freq.data,
                                            Img_ra,Img_dec,
@@ -37,17 +38,22 @@ def get_lens_param(is_td=True, **kwargs):
     #------------return numpy values---------------#
     hp_tilde_lensed, hc_tilde_lensed = get_lensed_gws(Fmag, hp.data, hc.data)
 
-    #------------convert to pycbc.TimeSeries---------------#
-    
-    hp_tilde_lensed = TimeSeries(hp_tilde_lensed, delta_t=hp.delta_t)
-    hp_tilde_lensed.start_time += hp.start_time
-    hc_tilde_lensed = TimeSeries(hc_tilde_lensed, delta_t=hc.delta_t)
-    hc_tilde_lensed.start_time += hc.start_time
-    return hp_tilde_lensed, hc_tilde_lensed  
+    #------------convert to pycbc.TimeSeries/FrequencySeries---------------#
+    if is_td:
+        hp_tilde_lensed = TimeSeries(hp_tilde_lensed, delta_t=hp.delta_t)
+        hp_tilde_lensed.start_time += hp.start_time
+        hc_tilde_lensed = TimeSeries(hc_tilde_lensed, delta_t=hc.delta_t)
+        hc_tilde_lensed.start_time += hc.start_time
+        return hp_tilde_lensed, hc_tilde_lensed  
+    else:
+        hp_tilde_lensed = FrequencySeries(hp_tilde_lensed, delta_f=hp.delta_f, dtype=np.float64)
+        hp_tilde_lensed.start_time += hp.start_time
+        hc_tilde_lensed = FrequencySeries(hc_tilde_lensed, delta_f=hc.delta_f, dtype=np.float64)
+        hc_tilde_lensed.start_time += hc.start_time
+        return hp_tilde_lensed, hc_tilde_lensed  
 
 def lensed_gw_fd(**kwargs):
-    hp_tilde_lensed, hc_tilde_lensed = get_lens_param(is_td=False,**kwargs)
-    return hp_tilde_lensed.to_frequencyseries(), hc_tilde_lensed.to_frequencyseries()
+    return get_lens_param(is_td=False,**kwargs)
 
 def lensed_gw_td(**kwargs):
     return get_lens_param(is_td=True,**kwargs)
