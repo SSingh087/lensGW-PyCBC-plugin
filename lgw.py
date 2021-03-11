207119d126c3b3c0e4a6a9b785dbc86da626d197
Shashwat Singh <66710757+SSingh087@users.noreply.github.com>
2021-03-11 12:49:20 +0530

    Update lgw.py

--- lgw.py
+++ lgw.py
@@ -16,10 +16,16 @@ def get_lens_param(is_td=True, **kwargs):
     zL = kwargs['zL']
     mL = kwargs['mL']
     lens_model_list = kwargs['lens_model_list']
-    Img_ra, Img_dec, source_pos_x, source_pos_y,\
-                _, _, eta0, eta1, _, kwargs_lens_list = lens_waveform_model(None).eval_param(
-                                                            y0,y1,l0,l1,zS,zL,mL,lens_model_list)
-                
+    mL,l0,l1 = array(mL,dtype=float64), array(l0,dtype=float64), array(l1,dtype=float64)
+    if len(mL>1):
+        Img_ra, Img_dec, source_pos_x, source_pos_y, _, _, _, _,\
+        _, kwargs_lens_list = lens_waveform_model(None).eval_param(
+                                                        y0,y1,l0,l1,zS,zL,mL,lens_model_list)
+    elif len(mL)==1:
+        Img_ra, Img_dec, source_pos_x, source_pos_y, _, _, _, _,\
+        lens_model_list, kwargs_lens_list=lens_waveform_model(None).eval_param(
+                                                        y0,y1,l0,l1,zS,zL,mL,lens_model_list)
+
     if "approximant" in kwargs:
         kwargs.pop("approximant")
     if is_td:
