fe55409c5a045e0c5cf695b864046cc4e6bedc94
Shashwat Singh <66710757+SSingh087@users.noreply.github.com>
2021-04-21 12:34:20 +0530

    Update lgw.py

--- lgw.py
+++ lgw.py
@@ -6,15 +6,13 @@ from pycbc.types.frequencyseries import FrequencySeries
 from pycbc import waveform
 from numpy import array, float64
 
-def get_lens_param(zS, zL, y0, y1, is_td=True, **kwargs):
-    
-    l0 = kwargs['l0']
-    l1 = kwargs['l1']
-    mL = kwargs['mL']
+def get_lens_param(mL, l0, l1, zS, zL, y0, y1, is_td=True, **kwargs):
     optim = kwargs['optim']
     lens_model_list = kwargs['lens_model_list']
-    mL,l0,l1 = array(mL,dtype=float64), array(l0,dtype=float64), array(l1,dtype=float64)
-    if len(mL>1):
+    mL, l0, l1 = array(mL,dtype=float64), array(l0,dtype=float64), array(l1,dtype=float64)
+    print(mL, len(mL), mL.shape)
+    if mL.shape[1]>1:
+        mL, l0, l1 = mL.squeeze(axis=0), l0.squeeze(axis=0), l1.squeeze(axis=0)
         Img_ra, Img_dec, source_pos_x, source_pos_y, _, _, _, _,\
         _, kwargs_lens_list = lens_waveform_model(None).eval_param(
                                                     y0,y1,l0,l1,zS,zL,mL,lens_model_list,optim)
@@ -48,9 +46,11 @@ def get_lens_param(zS, zL, y0, y1, is_td=True, **kwargs):
     else:
         return hp_tilde_lensed, hc_tilde_lensed, hp.delta_f
 
-def lensed_gw_fd(zS=2.0, zL=0.5, y0=0.3, y1=0.3,**kwargs):
-    hp_tilde_lensed, hc_tilde_lensed, delta_f = get_lens_param(zS, zL, y0, y1, is_td=False,**kwargs)
+def lensed_gw_fd(mL=1e8, l0=0.5, l1=0, zS=2.0, zL=0.5, y0=0.3, y1=0.3,**kwargs):
+    mL, l0, l1 = [mL], [l0], [l1]
+    hp_tilde_lensed, hc_tilde_lensed, delta_f = get_lens_param(mL, l0, l1, zS, zL, y0, y1, is_td=False,**kwargs)
     return hp_tilde_lensed.to_frequencyseries(delta_f=delta_f), hc_tilde_lensed.to_frequencyseries(delta_f=delta_f)
 
-def lensed_gw_td(zS=2.0, zL=0.5, y0=0.3, y1=0.3, **kwargs):
-    return get_lens_param(zS, zL, y0, y1, is_td=True,**kwargs)
+def lensed_gw_td(mL=1e8, l0=0.5, l1=0, zS=2.0, zL=0.5, y0=0.3, y1=0.3, **kwargs):
+    mL, l0, l1 = [mL], [l0], [l1]
+    return get_lens_param(mL, l0, l1, zS, zL, y0, y1, is_td=True,**kwargs)
